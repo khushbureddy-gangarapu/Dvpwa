1d98ad93c089a681379908e844ccfafaadb18c7a
khushbureddy-gangarapu <104974373+khushbureddy-gangarapu@users.noreply.github.com>
2022-05-12 15:44:25 +0100

    Update new_fuzzer.py

--- .clusterfuzzlite/new_fuzzer.py
+++ .clusterfuzzlite/new_fuzzer.py
@@ -2,9 +2,9 @@ import subprocess
 
 devnull = open("/dev/null", "w")
 def run():
-        ps = subprocess.Popen(["/bin/gzip", "-h"], shell=False, stdout=devnull)
-        ps.wait()
+    ps = subprocess.Popen(["/bin/gzip", "-h"], shell=False, stdout=devnull)
+    ps.wait()
 
 if __name__ == "__main__":
-        for x in range(0,1000):
-                run()
+    for x in range(0,1000):
+        run()
