96da34cceb67bd0e3b7e2fefc66dbf3bea32c6b6
khushbureddy-gangarapu <104974373+khushbureddy-gangarapu@users.noreply.github.com>
2022-05-13 10:08:42 +0100

    Update new_fuzzer.py

--- .clusterfuzzlite/new_fuzzer.py
+++ .clusterfuzzlite/new_fuzzer.py
@@ -1,10 +1,17 @@
-import subprocess
+import atheris
+with atheris.instrument_imports():
+  import sys
+  import tensorflow as tf
 
-devnull = open("/dev/null", "w")
-def run():
-    ps = subprocess.Popen(["/bin/gzip", "-h"], shell=False, stdout=devnull)
-    ps.wait()
+
+def TestOneInput(data):
+  tf.constant(data)
+
+
+def main():
+  atheris.Setup(sys.argv, TestOneInput, enable_python_coverage=True)
+  atheris.Fuzz()
+
 
 if __name__ == "__main__":
-    for x in range(0,1000):
-        run()
+  main()
